60c851ce21f96018adaf1e68fec8d4b35082dba1
Nick Savage <nick@nicksavage.ca>
2025-09-13 19:14:54 -0400

    Improving TaskPageOptionsMenu

--- zettelkasten-front/src/components/tasks/TaskPageOptionsMenu.tsx
+++ zettelkasten-front/src/components/tasks/TaskPageOptionsMenu.tsx
@@ -4,6 +4,7 @@ import { SearchTagDropdown } from "../../components/tags/SearchTagDropdown";
 import { Tag } from "../../models/Tags";
 import { BulkTaskDateDisplay } from "./BulkTaskDateDisplay";
 import { Task } from "../../models/Task";
+import { Menu } from "@headlessui/react";
 
 interface TaskPageOptionsMenu {
   tags: Tag[];
@@ -16,43 +17,57 @@ export function TaskPageOptionsMenu({
   handleTagClick,
   tasks,
 }: TaskPageOptionsMenu) {
-  const [showMenu, setShowMenu] = useState<boolean>(false);
   const { setRefreshTasks } = useTaskContext();
   const [showTagMenu, setShowTagMenu] = useState<boolean>(false);
   const [showBulkEdit, setShowBulkEdit] = useState<boolean>(false);
 
-  function toggleMenu() {
-    setShowMenu(!showMenu);
-    setShowTagMenu(false);
+  function toggleTagMenu() {
+    setShowTagMenu(true);
     setShowBulkEdit(false);
   }
 
-  function toggleTagMenu() {
-    setShowMenu(false);
-    setShowTagMenu(true);
-  }
-
   function toggleBulkEdit() {
-    setShowMenu(false);
+    setShowTagMenu(false);
     setShowBulkEdit(true);
   }
 
   return (
     <div className="relative">
-      <div className="dropdown">
-        <button
-          onClick={toggleMenu}
-          className="font-semibold rounded focus:outline-none focus:ring-2 focus:ring-offset-2 bg-palette-dark text-white hover:bg-palette-darkest focus:ring-blue-500 px-4 py-2"
-        >
-          Actions
-        </button>
-        {showMenu && (
-          <div className="popup-menu w-64 absolute right-0">
-            <button onClick={toggleTagMenu}>Add Tags</button>
-            <button onClick={toggleBulkEdit}>Bulk Edit Date</button>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="font-semibold rounded focus:outline-none focus:ring-2 focus:ring-offset-2 bg-palette-dark text-white hover:bg-palette-darkest focus:ring-blue-500 px-4 py-2">
+            Actions
+          </Menu.Button>
+        </div>
+        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={toggleTagMenu}
+                  className={`${
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                  } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                >
+                  Add Tags
+                </button>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  onClick={toggleBulkEdit}
+                  className={`${
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'
+                  } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                >
+                  Bulk Edit Date
+                </button>
+              )}
+            </Menu.Item>
           </div>
-        )}
-      </div>
+        </Menu.Items>
+      </Menu>
 
       {showTagMenu && (
         <SearchTagDropdown
