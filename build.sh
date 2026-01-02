14ab81be13f75d5924896c56ba142127e33b9de0
Nick Savage <nick@nicksavage.ca>
2026-01-02 09:35:54 -0500

    Updating build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+set -e
+
 source .env
 
-sudo docker-compose build
+sudo docker compose build
+
+sudo docker compose push
+
+cd /mnt/nas-2-fast-data/config/zettelgarden
+sudo docker compose up -d
