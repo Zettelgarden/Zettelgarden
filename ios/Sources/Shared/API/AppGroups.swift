a253b2861261d9f40fc5efcbb1f76138046bcfd4
Nick Savage <nick@nicksavage.ca>
2026-01-09 14:45:03 -0500

    Remove old projects

--- ios/Sources/Shared/API/AppGroups.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public enum AppGroups: String {
-    case primary = "group.com.zettelgarden"
-
-    public var identifier: String { rawValue }
-}
--- ios/Sources/Shared/Models/AppConfiguration+Defaults.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public extension AppConfiguration {
-    static let empty = AppConfiguration(baseURL: nil, apiToken: "", defaultDestination: .card)
-}
