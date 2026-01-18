9ab24ee73d8175457de7ad703a054bc2560a04cb
Nick Savage <nick@nicksavage.ca>
2026-01-18 09:49:37 -0500

    Turn off validation temporarily

--- go-backend/pkg/config/config.go
+++ go-backend/pkg/config/config.go
@@ -21,9 +21,9 @@ func LoadConfig() Config {
 	}
 
 	// Check for validation errors and panic if any found
-	if !config.Server.DevMode {
-		panicOnValidationErrors()
-	}
+	//	if !config.Server.DevMode {
+	//panicOnValidationErrors()
+	//}
 
 	// Set global instance for service access
 	globalConfig = &config
