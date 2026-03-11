3ddbf6e7d8c0e98656c0b316033eac7ef12f3acc
Nick Savage <nick@nicksavage.ca>
2026-03-10 21:05:31 -0400

    Add routes for file tag management endpoints

--- go-backend/routes/files.go
+++ go-backend/routes/files.go
@@ -12,4 +12,10 @@ func RegisterFileRoutes(r *mux.Router, h *handlers.Handler) {
 	addProtectedRoute(r, h, "/api/files/{id}", h.EditFileMetadataRoute, "PATCH")
 	addProtectedRoute(r, h, "/api/files/{id}", h.DeleteFileRoute, "DELETE")
 	addProtectedRoute(r, h, "/api/files/download/{id}", h.DownloadFileRoute, "GET")
+
+	// File tag management routes
+	addProtectedRoute(r, h, "/api/files/tags", h.CreateFileTagRoute, "POST")
+	addProtectedRoute(r, h, "/api/files/tags", h.GetUserFileTagsRoute, "GET")
+	addProtectedRoute(r, h, "/api/files/{id}/tags", h.TagFileRoute, "POST")
+	addProtectedRoute(r, h, "/api/files/{id}/tags/{tag_name}", h.UntagFileRoute, "DELETE")
 }
